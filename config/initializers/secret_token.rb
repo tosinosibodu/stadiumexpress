27ffc00e5d665142aa5b102d422b34c3c8ff6d0f
Jordan <jzarrilli@gmail.com>
2011-02-11 13:23:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stadiumexpress::Application.config.secret_token = '5661512dce750fbbf09d490e68e9f9b0847c51d6ab578177628583034c7e5a66d3adfe567241bed03084cdac4ccef227afc7326e7718e47a5ced9e76ec9f9d6f'
